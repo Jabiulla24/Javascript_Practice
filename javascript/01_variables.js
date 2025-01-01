727f9f550aa8233a4c5409c28ecb873d29a23468
Jabiulla Mulla <jabiulla24@gmail.com>
2025-01-01 18:16:25 +0000

    Variables

---
+++ javascript/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=1234;
+let accountName="Rajesh";
+var accountEmail='mike@gmail.com';
+accountCity="Banglore";// We can use in Javascript but not recomended
+let accStatus; // This is not defined if we print it gives undefined
+//accountId=2344; Cannot assign any other values for Const
+console.log('a',accountId);
+
+console.table([accountId,accountName,accountEmail,accountCity,accStatus]);
