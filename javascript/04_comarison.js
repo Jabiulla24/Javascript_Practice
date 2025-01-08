cc50f623797d85adddcdc03144e26453fe3d1a5d
Jabiulla Mulla <jabiulla24@gmail.com>
2025-01-08 07:27:02 +0000

    comparison operator

---
+++ javascript/04_comarison.js
@@ -0,0 +1,19 @@
+//In this we will see comparison operators like >,<,>=,<=,==,!=,===,!==
+
+console.log(2>1);
+console.log(3<7);
+console.log(2>=6);
+console.log(2<=5);
+
+console.log("2" == 2);// It gives true as there is data type conversion happens it is converted to 2 and 2 ==2 so true
+console.log("2" === 2);// It give false as this is === strict data type check is checked as string is compared with number its false
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);//True as null is converted to 0 in Boolean data type conversion as true
+
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined<=0);
+
